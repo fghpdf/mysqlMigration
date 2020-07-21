3486c00a65530b9699fc07fd9f8344ea4090ea3f
fghpdf <quxiangxuanqxx@gmail.com>
2020-07-21 16:01:58 +0800

    feat: add some constants:       * haRowType       * haOption
          add table struct:
          * table
          * table options

---
+++ frm/table.go
@@ -0,0 +1,29 @@
+package frm
+
+import "fghpdf.com/mysqlMigration/frm/constants"
+
+type table struct {
+	Name         string
+	MySQLVersion MySQLVersion
+	Charset      string
+	TableOptions tableOptions
+	Columns      []column
+}
+
+type tableOptions struct {
+	Connection    byteSlice
+	Engine        string
+	Charset       string
+	MinRows       uint64
+	MaxRows       uint64
+	AvgRowLength  uint64
+	HaOption      int
+	RowFormat     constants.HaRowType
+	keyBlockSize  uint64
+	Comment       string
+	PartitionInfo byteSlice
+}
+
+type column struct {
+	Name string
+}
